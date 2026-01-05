b33a2d4e9bf4e38abe0c124eecbb224d89427a26
Botgamer11 <75205800+Botgamer11@users.noreply.github.com>
2026-01-05 16:40:29 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"  // сканирует все JS/JSX файлы в src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
